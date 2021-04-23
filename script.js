020e1155f40350a3e9d671754b42d14504936ba1
Lukaszzaraska <79981915+Lukaszzaraska@users.noreply.github.com>
2021-04-23 18:07:11 +0200

    base 0.2

---
+++ script.js
@@ -0,0 +1,46 @@
+function change(elm) {
+    if ( elm.marked) {
+      elm.src = 'aktualnosci.png'; 
+      elm.marked = false;
+   } else {
+      elm.src = 'aktualnosci_click.png';
+      elm.marked = true;
+   }
+  }
+
+  function change2(elm) {
+    if ( elm.marked) {
+      elm.src = 'archiwum.png'; 
+      elm.marked = false;
+   } else {
+      elm.src = 'archiwum_click.png';
+      elm.marked = true;
+   }
+  }
+  function change3(elm) {
+    if ( elm.marked) {
+      elm.src = 'terminarz.png'; 
+      elm.marked = false;
+   } else {
+      elm.src = 'terminarz_click.png';
+      elm.marked = true;
+   }
+  }
+  function change4(elm) {
+    if ( elm.marked) {
+      elm.src = 'wyniki.png'; 
+      elm.marked = false;
+   } else {
+      elm.src = 'wyniki_click.png';
+      elm.marked = true;
+   }
+  }
+  function change5(elm) {
+    if ( elm.marked) {
+      elm.src = 'onas.png'; 
+      elm.marked = false;
+   } else {
+      elm.src = 'onas_click.png';
+      elm.marked = true;
+   }
+  }
