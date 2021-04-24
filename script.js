6e9dbfd2bdce578ce21db0cf58984be60176d81c
Lukaszzaraska <79981915+Lukaszzaraska@users.noreply.github.com>
2021-04-24 17:27:19 +0200

    Live_base0.3

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 function change(elm) {
+    var naszaWarstwa = document.getElementById("aktualnosci");
     if ( elm.marked) {
       elm.src = 'aktualnosci.png'; 
       elm.marked = false;
+      naszaWarstwa.style.display = "none";
    } else {
       elm.src = 'aktualnosci_click.png';
       elm.marked = true;
+      naszaWarstwa.style.display = "block";
    }
   }
 
   function change2(elm) {
+    var naszaWarstwa = document.getElementById("archiwum");
     if ( elm.marked) {
       elm.src = 'archiwum.png'; 
       elm.marked = false;
+      naszaWarstwa.style.display = "none";
    } else {
       elm.src = 'archiwum_click.png';
       elm.marked = true;
+      naszaWarstwa.style.display = "block";
    }
   }
   function change3(elm) {
+    var naszaWarstwa = document.getElementById("terminarz");
     if ( elm.marked) {
       elm.src = 'terminarz.png'; 
       elm.marked = false;
+      naszaWarstwa.style.display = "none";
    } else {
       elm.src = 'terminarz_click.png';
       elm.marked = true;
+      naszaWarstwa.style.display = "block";
    }
   }
   function change4(elm) {
+    var naszaWarstwa = document.getElementById("wyniki");
     if ( elm.marked) {
       elm.src = 'wyniki.png'; 
       elm.marked = false;
+      naszaWarstwa.style.display = "none";
    } else {
       elm.src = 'wyniki_click.png';
       elm.marked = true;
+      naszaWarstwa.style.display = "block";
    }
   }
   function change5(elm) {
+    var naszaWarstwa = document.getElementById("onas");
     if ( elm.marked) {
       elm.src = 'onas.png'; 
       elm.marked = false;
+      naszaWarstwa.style.display = "none";
    } else {
       elm.src = 'onas_click.png';
       elm.marked = true;
+      naszaWarstwa.style.display = "block";
    }
   }
